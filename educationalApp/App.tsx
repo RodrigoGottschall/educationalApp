7c626ce560118982d873c1668f385c8208dbe4e6
RodrigoGottschall <ro_criscuolo@hotmail.com>
2024-05-21 18:57:34 -0300

    logo import correction

--- educationalApp/App.tsx
+++ educationalApp/App.tsx
@@ -1,20 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
-import LoadingScreen from './components/LoadingScreen'; // Importe o componente
-import logoInnovateTech from './assets/logo.png'; // Importe o logo
+import { View, Image } from 'react-native';
+import LoadingScreen from './components/LoadingScreen';
 
-const HomeScreen: React.FC = () => {
+const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
-    }, 2000); 
+    }, 2000);
   }, []);
 
   return (
-    <View>
-      <LoadingScreen logoSource={logoInnovateTech} />
+    <View style={{ flex: 1 }}>
+      {isLoading ? (
+        <LoadingScreen logoSource={require('./assets/logo.png')} />
+      ) : (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Image
+            source={require('./assets/logo.png')}
+            style={{ width: 200, height: 200 }}
+          />
+        </View>
+      )}
     </View>
   );
 };
+
+export default App;
